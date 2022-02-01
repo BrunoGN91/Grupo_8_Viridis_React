f238bdd04588aed1fcb765bc3bbd112240264456
Franmon1 <fran.montero95@gmail.com>
2022-01-31 21:19:12 -0300

    Corrección de componente Last User

--- src/components/LastUser.js
+++ src/components/LastUser.js
@@ -15,18 +15,24 @@ function LastUser (){
 	}, [])
 
     return (
+        <div className="col-lg-6 mb-4">
+        <div className="card shadow mb-4">
+            <div className="card-header py-3">
+                <h5 className="m-0 font-weight-bold text-gray-800">Last user in Database</h5>
+            </div>
             <div className="card-body">
                 <div className="text-center">
-                  
-                    <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={`http://localhost:3001/images/users/${users[0].user_image}`} alt={''}/>
-                    
-                    <p>{users[0].name}</p>
+                <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={`http://localhost:3001/images/users/${users[0].user_image}`} alt={''}/>
                 </div>
-                <p>{users[0].email}</p>
+                <p><b>User:</b> {users[0].name}</p>
+                <p><b>Email:</b> {users[0].email}</p>
             </div>
-    )
+        </div>
+        </div>
+        
             
-    
+    )
+
 }
 
 export default LastUser;
