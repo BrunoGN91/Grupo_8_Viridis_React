63c320b948e08e19e92cc73799d86d8da23b0a22
Franmon1 <fran.montero95@gmail.com>
2022-02-02 01:22:10 -0300

    Corrección de Last User

--- src/components/LastUser.js
+++ src/components/LastUser.js
@@ -20,13 +20,20 @@ function LastUser (){
             <div className="card-header py-3">
                 <h5 className="m-0 font-weight-bold text-gray-800">Last user in Database</h5>
             </div>
-            <div className="card-body">
+            {
+                users.length>0?
+                <div className="card-body">
                 <div className="text-center">
-                <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={`http://localhost:3001/images/users/${users[0].user_image}`} alt={''}/>
+                <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={`http://localhost:3001/images/users/${users[0].user_image??null}`} alt={''}/>
                 </div>
-                <p><b>User:</b> {users[0].name}</p>
+                <p><b>ID:</b> {users[0].id} </p>
+                <p><b>Username:</b> {users[0].name}</p>
                 <p><b>Email:</b> {users[0].email}</p>
+                <a className="btn btn-danger" target="_blank" rel="nofollow" href={`http://localhost:3001/users/profile/`} >View user Detail</a>
             </div>
+            :null
+            }
+            
         </div>
         </div>
         
