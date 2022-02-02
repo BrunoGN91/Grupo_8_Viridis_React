0e25b11e408c06a7d5e86a257cbd63b9a985e2f4
Franmon1 <fran.montero95@gmail.com>
2022-02-02 18:49:29 -0300

    Last User arreglado

--- src/components/LastUser.js
+++ src/components/LastUser.js
@@ -14,6 +14,9 @@ function LastUser (){
 		)
 	}, [])
 
+    let lastUser = users.slice(-1)
+    console.log(lastUser)
+
     return (
         <div className="col-lg-6 mb-4">
         <div className="card shadow mb-4">
@@ -21,18 +24,23 @@ function LastUser (){
                 <h5 className="m-0 font-weight-bold text-gray-800">Last user in Database</h5>
             </div>
             {
-                users.length>0?
-                <div className="card-body">
+                lastUser.map((user, i) => {   
+
+                return(
+                    <div className="card-body" key={i}>
+
                 <div className="text-center">
-                <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={`http://localhost:3001/images/users/${users[0].user_image??null}`} alt={''}/>
+                <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={`http://localhost:3001/images/users/${user.user_image}`} alt={''}/>
                 </div>
-                <p><b>ID:</b> {users[0].id} </p>
-                <p><b>Username:</b> {users[0].name}</p>
-                <p><b>Email:</b> {users[0].email}</p>
-                <a className="btn btn-danger" target="_blank" rel="nofollow" href={`http://localhost:3001/users/profile/`} >View user Detail</a>
+                <p><b>ID:</b> {user.id} </p>
+                <p><b>Username:</b> {user.name}</p>
+                <p><b>Email:</b> {user.email}</p>
+                <a className="btn btn-danger" target="_blank" rel="nofollow" href={`http://localhost:3001/users/userDetail/${user.id}`} >View user Detail</a>
             </div>
-            :null
-            }
+                  )})
+}
+        
+        
             
         </div>
         </div>
